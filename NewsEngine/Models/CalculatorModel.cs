30cda9b75b10bab00412164ae97112703b2b5c47
d.chykin <d.chykin@gmail.com>
2015-03-22 17:00:35 +0300

    Lesson02

---
+++ NewsEngine/Models/CalculatorModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsEngine.Models
+{
+    public class CalculatorModel
+    {
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public CalculatorOperation Operation { get; set; }
+
+        public double GetResult()
+        {
+            return GetResult(X, Y, Operation);
+        }
+
+        private double GetResult(double x, double y, CalculatorOperation operation)
+        {
+            double result = 0d;
+            switch(operation)
+            {
+                case CalculatorOperation.Add:
+                    result = x + y;
+                    break;
+                case CalculatorOperation.Division:
+                    result = x / y;
+                    break;
+                case CalculatorOperation.Multiplication:
+                    result = x * y;
+                    break;
+                case CalculatorOperation.Subtraction:
+                    result = x - y;
+                    break;
+            }
+            return result;
+        }
+
+        public enum CalculatorOperation
+        {
+            Add, Subtraction, Multiplication, Division
+        }
+    }
+}
