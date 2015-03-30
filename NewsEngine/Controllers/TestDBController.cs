a54a8fb1fd1f6d8114eb00153b367e278959b0ec
d.chykin <d.chykin@gmail.com>
2015-03-30 09:09:26 +0300

    Lesson04

--- NewsEngine/Controllers/TestDBController.cs
+++ NewsEngine/Controllers/TestDBController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using NewsEngine.Models;
@@ -19,26 +21,86 @@ namespace NewsEngine.Controllers
         [HttpGet]
         public ActionResult AddMessage()
         {
-            return View(new Message());
+            return View();
         }
 
         [HttpPost]
         public ActionResult AddMessage(Message message)
         {
-            if (message.MessageTitle == null || message.Text == null)
-                throw new ArgumentNullException();
+            testDB.Messages.Add(message);
+            testDB.SaveChanges();
+            return RedirectToAction("ShowMessages");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteMessage(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = testDB.Messages.Find(id);
+            if (message == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
             else
             {
-                testDB.Messages.Add(message);
+                testDB.Messages.Remove(message);
                 testDB.SaveChanges();
-                return RedirectToAction("ShowMessages");
             }
+            return RedirectToAction("ShowMessages");
+        }
+        
+        [HttpGet]
+        public ActionResult EditMessage(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Message message = testDB.Messages.Find(id);
+            if (message == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            return View(message);
+        }
+
+        [HttpPost]
+        public ActionResult EditMessage(Message message)
+        {
+            if (message == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            int? id = (testDB.Messages.Find(message.Id)).Id;
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            Message newMessage = testDB.Messages.Find(message.Id);
+            newMessage.Text = message.Text;
+            newMessage.MessageTitle = message.MessageTitle;
+            testDB.Entry(newMessage).State = EntityState.Modified;
+            //testDB.Entry(message).State = EntityState.Modified;
+            testDB.SaveChanges();
+            return RedirectToAction("ShowMessages");
         }
 
         public ActionResult ShowMessages()
         {
             ViewBag.Messages = testDB.Messages.OrderBy(messages=>messages.CurrentDate).ToList<Message>();
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                testDB.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
--- NewsEngine/Models/Message.cs
+++ NewsEngine/Models/Message.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Web;
 
 namespace NewsEngine.Models
 {
     public class Message
     {
-        public int id { get; set; }
+        public int Id { get; set; }
 
+        [Display(Name = "Текст")]
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.MultilineText)]
         public String Text { get; set; }
+        [Display(Name = "Заголовок")]
+        [Required]
+        [StringLength(25, MinimumLength = 5, ErrorMessage = "Длина титула должна быть от 5 до 15 символов")]
         public String MessageTitle { get; set; }
         public DateTime CurrentDate { get; private set; }
 
